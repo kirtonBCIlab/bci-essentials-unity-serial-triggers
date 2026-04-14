10759e53db2a4cd4a666f1aa40c49b458be31042
Twig <git@twig.gay>
2026-04-14 11:50:18 -0600

    Removed core package components

--- Samples~/SSVEP Tile Navigation/Scripts/Visuals/Collectable.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[RequireComponent(typeof(SpriteRenderer))]
-public class Collectable : GridSortedDisplay
-{
-    public float CollectionDelay = 0.2f;
-
-    public void Collect() => Destroy(gameObject, CollectionDelay);
-}
